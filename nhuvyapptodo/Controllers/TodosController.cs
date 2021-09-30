01049e11dc893593ea5938ea7b8cab2e01c9df2c
phucnguyen17072000k121@gmail.com <phucnguyen17072000k121@gmail.com>
2021-09-30 02:38:45 -0700

    Create list of todo and return it to View

--- nhuvyapptodo/Controllers/TodosController.cs
+++ nhuvyapptodo/Controllers/TodosController.cs
@@ -1,5 +1,7 @@
 ﻿using nhuvyapptodo.Models;
 using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Web.Mvc;
 
 namespace nhuvyapptodo.Controllers
@@ -9,13 +11,14 @@ namespace nhuvyapptodo.Controllers
         // GET: Todos
         public ActionResult Index()
         {
-            Todo todo = new Todo()
+            List<Todo> todos = new List<Todo>()
             {
-                Id = 1,
-                Description = "Kill Bill",
-                DueDate = new DateTime(2008, 12, 20)
-        };
-            return View(todo);
+                new Todo(){Id = 1, Description = "Kill Bill", DueDate =  new DateTime(2008, 12, 20)},
+                new Todo(){Id = 2, Description = "Kill Bill 2", DueDate =  new DateTime(2012, 1, 22)},
+                new Todo(){Id = 3, Description = "John Wick", DueDate =  new DateTime(2020, 01, 22)}
+
+            };
+            return View(todos);
         }
     }
 }
