80f79f10eda1b4c9aa16848e5b4f673d14412098
Admin <Admin@ADMIN>
2021-09-29 23:41:12 -0700

    Add project files.

---
+++ nhuvyapptodo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(nhuvyapptodo.Startup))]
+namespace nhuvyapptodo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
